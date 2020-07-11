c5e816dfee8d36110775378c74106ffa75123655
Jeff <jmdonios@gmail.com>
2020-07-11 14:41:25 +1000

    ADDED: container component

---
+++ src/components/container.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Container ({ children }) {
+	return <div className="container mx-auto px-5">{children}</div>;
+}
+
+Container.propTypes = { children: PropTypes.object };
